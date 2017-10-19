cce474c40887d7190f672e00b41cd7596e8d7be1
wangyongfang123 <1412633624@qq.com>
2017-10-19 15:04:05 +0800

    Login设计

--- QtNetworkClient/GeneratedFiles/ui_userlogin.h
+++ QtNetworkClient/GeneratedFiles/ui_userlogin.h
@@ -14,19 +14,100 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QDialog>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QVBoxLayout>
+#include "image.h"
 
 QT_BEGIN_NAMESPACE
 
 class Ui_UserLogin
 {
 public:
+    QVBoxLayout *verticalLayout_3;
+    QVBoxLayout *verticalLayout_4;
+    QGroupBox *groupBox;
+    Image *m_Image;
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *horizontalLayout_3;
+    QPushButton *pushButton_2;
+    QLineEdit *lineEdit_2;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *pushButton;
+    QLineEdit *lineEdit;
+    QPushButton *pushButton_3;
 
     void setupUi(QDialog *UserLogin)
     {
         if (UserLogin->objectName().isEmpty())
             UserLogin->setObjectName(QStringLiteral("UserLogin"));
-        UserLogin->resize(400, 300);
+        UserLogin->resize(546, 428);
+        verticalLayout_3 = new QVBoxLayout(UserLogin);
+        verticalLayout_3->setSpacing(6);
+        verticalLayout_3->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_3->setObjectName(QStringLiteral("verticalLayout_3"));
+        verticalLayout_4 = new QVBoxLayout();
+        verticalLayout_4->setSpacing(6);
+        verticalLayout_4->setObjectName(QStringLiteral("verticalLayout_4"));
+        groupBox = new QGroupBox(UserLogin);
+        groupBox->setObjectName(QStringLiteral("groupBox"));
+        m_Image = new Image(groupBox);
+        m_Image->setObjectName(QStringLiteral("m_Image"));
+        m_Image->setGeometry(QRect(110, 10, 311, 256));
+        m_Image->setMinimumSize(QSize(160, 160));
+        m_Image->setBaseSize(QSize(64, 64));
+
+        verticalLayout_4->addWidget(groupBox);
+
+
+        verticalLayout_3->addLayout(verticalLayout_4);
+
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setSpacing(6);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setSpacing(6);
+        horizontalLayout_3->setObjectName(QStringLiteral("horizontalLayout_3"));
+        pushButton_2 = new QPushButton(UserLogin);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+
+        horizontalLayout_3->addWidget(pushButton_2);
+
+        lineEdit_2 = new QLineEdit(UserLogin);
+        lineEdit_2->setObjectName(QStringLiteral("lineEdit_2"));
+
+        horizontalLayout_3->addWidget(lineEdit_2);
+
+
+        verticalLayout->addLayout(horizontalLayout_3);
+
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        pushButton = new QPushButton(UserLogin);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+
+        horizontalLayout->addWidget(pushButton);
+
+        lineEdit = new QLineEdit(UserLogin);
+        lineEdit->setObjectName(QStringLiteral("lineEdit"));
+
+        horizontalLayout->addWidget(lineEdit);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+
+        verticalLayout_3->addLayout(verticalLayout);
+
+        pushButton_3 = new QPushButton(UserLogin);
+        pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
+
+        verticalLayout_3->addWidget(pushButton_3);
+
 
         retranslateUi(UserLogin);
 
@@ -36,6 +117,11 @@ public:
     void retranslateUi(QDialog *UserLogin)
     {
         UserLogin->setWindowTitle(QApplication::translate("UserLogin", "UserLogin", Q_NULLPTR));
+        groupBox->setTitle(QString());
+        m_Image->setText(QString());
+        pushButton_2->setText(QApplication::translate("UserLogin", "AccountNumber", Q_NULLPTR));
+        pushButton->setText(QApplication::translate("UserLogin", "Password    ", Q_NULLPTR));
+        pushButton_3->setText(QApplication::translate("UserLogin", "Login", Q_NULLPTR));
     } // retranslateUi
 
 };
--- QtNetworkClient/userlogin.cpp
+++ QtNetworkClient/userlogin.cpp
@@ -1,9 +1,13 @@
 #include "userlogin.h"
-
+#include"image.h"
+#include<QImageReader>
 UserLogin::UserLogin(QWidget *parent)
 	: QDialog(parent)
 {
 	ui.setupUi(this);
+	QImageReader ir(":/menu/Resources/default_user_image.jpg");
+	QPixmap image = QPixmap::fromImageReader(&ir);
+	ui.m_Image->setPixmap(image.scaled(ui.m_Image->size()));
 }
 
 UserLogin::~UserLogin()
